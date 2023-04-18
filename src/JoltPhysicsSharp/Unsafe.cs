a8f423de36f3b35bec12ec335b56cd1822f08447
Velctor <Velctor@qq.com>
2023-04-18 20:10:29 +0800

    change .cs encoding to windows 1252

---
+++ src/JoltPhysicsSharp/Unsafe.cs
@@ -0,0 +1,9 @@
+﻿// Copyright © Amer Koleci and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+#if !NET
+namespace JoltPhysicsSharp;
+static class Unsafe
+{
+    public static unsafe ref T AsRef<T>(void* source) where T : unmanaged => ref *(T*)source;
+}
+#endif
