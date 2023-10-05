e14f3c53dc38cef0c8d06605aae65a7248f8d375
Amer Koleci <amerkoleci@gmail.com>
2023-10-05 08:55:32 +0200

    FIX bug with MassProperties.ScaleToMass.

--- src/JoltPhysicsSharp/MassProperties.cs
+++ src/JoltPhysicsSharp/MassProperties.cs
@@ -4,17 +4,20 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using static JoltPhysicsSharp.JoltApi;
 
 namespace JoltPhysicsSharp;
 
 [StructLayout(LayoutKind.Sequential, Pack = 4)]
 public struct MassProperties : IEquatable<MassProperties>
 {
+    /// <summary>
     /// Mass of the shape (kg)
+    /// </summary>
 	public float Mass = 0.0f;
 
+    /// <summary>
     /// Inertia tensor of the shape (kg m^2)
+    /// </summary>
     public Matrix4x4 Inertia = default;
 
     public MassProperties()
@@ -38,28 +41,37 @@ public struct MassProperties : IEquatable<MassProperties>
     public static bool operator !=(MassProperties left, MassProperties right) => !left.Equals(right);
 
     /// <inheritdoc/>
-    public override bool Equals(object? obj) => obj is MassProperties handle && Equals(handle);
+    public override readonly bool Equals(object? obj) => obj is MassProperties handle && Equals(handle);
 
     /// <inheritdoc/>
-    public override int GetHashCode() => HashCode.Combine(Mass, Inertia);
+    public override readonly int GetHashCode() => HashCode.Combine(Mass, Inertia);
 
-    public void SetMassAndInertiaOfSolidBox(in Vector3 inBoxSize, float inDensity)
+    /// <summary>
+    /// Set the mass and inertia of a box with edge size inBoxSize and density inDensity
+    /// </summary>
+    /// <param name="boxSize"></param>
+    /// <param name="density"></param>
+    public void SetMassAndInertiaOfSolidBox(in Vector3 boxSize, float density)
     {
         // Calculate mass
-        Mass = inBoxSize.X * inBoxSize.Y * inBoxSize.Z * inDensity;
+        Mass = boxSize.X * boxSize.Y * boxSize.Z * density;
 
         // Calculate inertia
-        Vector3 size_sq = inBoxSize * inBoxSize;
+        Vector3 size_sq = boxSize * boxSize;
         Vector3 scale = (new Vector3(size_sq.Y, size_sq.X, size_sq.X) + new Vector3(size_sq.Z, size_sq.Z, size_sq.Y)) * (Mass / 12.0f);
         Inertia = Matrix4x4.CreateScale(scale);
     }
 
+    /// <summary>
+    /// Set the mass and scale the inertia tensor to match the mass
+    /// </summary>
+    /// <param name="mass"></param>
     public void ScaleToMass(float mass)
     {
         if (Mass > 0.0f)
         {
             // Calculate how much we have to scale the inertia tensor
-            float mass_scale = Mass / Mass;
+            float mass_scale = mass / Mass;
 
             // Update mass
             Mass = mass;
