42dcce07a56c83a97e74d173efd82482956fad44
Amer Koleci <amerkoleci@gmail.com>
2023-11-13 12:14:04 +0100

    Update JoltPhysics to https://github.com/jrouwe/JoltPhysics/commit/4ff703d11506290383fc82e926f1eaad728f0e01 with release https://github.com/jrouwe/JoltPhysics/releases/tag/v4.0.0

--- build/cmake_vs2022_x64.bat
+++ build/cmake_vs2022_x64.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake -B "vs2022_x64" -S "./../" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX:String="SDK" %*
+cmake -S "./../" -B "vs2022_x64" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX:String="SDK" %*
 echo Open vs2022_x64\JoltC.sln to build the project.
--- build/cmake_vs2022_x64_double.bat
+++ build/cmake_vs2022_x64_double.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake -B "vs2022_x64_double" -S "./../" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX:String="SDK" -DDOUBLE_PRECISION=ON %*
+cmake -S "./../" -B "vs2022_x64_double" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX:String="SDK" -DDOUBLE_PRECISION=ON %*
 echo Open vs2022_x64_double\JoltC.sln to build the project.
